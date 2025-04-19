611329126bc9ddc88b10bda2e32d0b48503b794f
Kristaps Irbe <A230484KI@rvt.lv>
2025-04-19 21:00:05 +0300

    Profile page spiežot uz "Your buy list" attēlo visas grāmatas, kurām user atzīmēja, ka vēlas nopirkt.

--- src/main/java/com/example/vadimaprojekts/controllers/ProfileController.java
+++ src/main/java/com/example/vadimaprojekts/controllers/ProfileController.java
@@ -102,7 +102,58 @@ public class ProfileController implements Initializable {
 
     @FXML
     public void onbuyListBtnClick(ActionEvent event) {
-        System.out.println("onbuyListBtnClick");
+        int bookCount = user.getBuyList().size();
+        List<String> bookId = new ArrayList<>(user.getBuyList());
+        anchorPane.getChildren().removeIf(node -> node instanceof ImageView);
+        anchorPane.getChildren().removeIf(node -> node instanceof Label);
+        int rows = (int) Math.ceil(bookCount / 3.0);
+        anchorPane.setPrefHeight(608 + (rows - 1) * 400);
+
+        books = new ImageView[bookCount];
+        labels = new Label[bookCount];
+        for (int i = 0; i < bookCount; i++) {
+            ImageView imageView = new ImageView();
+            Label label = new Label(bookService.getBookData(bookId.get(i)).getTitle());
+            imageView.setFitWidth(180);
+            imageView.setFitHeight(255);
+            label.setWrapText(true);
+            label.setStyle("-fx-font-size: 16; -fx-text-alignment: center;");
+            label.setAlignment(Pos.CENTER);
+            label.setPrefWidth(180);
+            label.setPrefHeight(50);
+
+            int row = i / 3;
+            int col = i % 3;
+
+            double x = 85 + col * 350;
+            double y = 70 + row * 400;
+
+            imageView.setX(x);
+            imageView.setY(y);
+            label.setLayoutX(x);
+            label.setLayoutY(y + 270);
+
+            try {
+                imageView.setImage(imageCache.getImage(bookService.getBookData(bookId.get(i)).getImageLinks()));
+            } catch (Exception e) {
+                System.out.println("Nesanaca");
+            }
+
+            label.setCursor(Cursor.HAND);
+            label.setOnMouseClicked(events -> {
+                try {
+                    session.setBook(bookService.getBookDataByTitle(label.getText()));
+                    switchToSceneService.switchToBookPage(label.getText());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+
+            books[i] = imageView;
+            labels[i] = label;
+            anchorPane.getChildren().add(imageView);
+            anchorPane.getChildren().add(label);
+        }
     }
 
     @FXML
